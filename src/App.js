d10405a39ada8d5da9563f682d34f2e8bbc58dcf
= <LHP6.arazzok@gmail.com>
2022-01-28 17:12:48 +0100

    add home page and navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 export default function App() {
 	return (
-		<div className="App">
-			<h1 className='display-1'>Home</h1>
-		</div>
-	);
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</>
+	)
 }
