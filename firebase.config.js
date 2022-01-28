8cf1229e339c45d0ded05a8a9795189b7ead5c67
= <LHP6.arazzok@gmail.com>
2022-01-28 16:10:39 +0100

    add env variables and link them with firebase config

--- firebase.config.js
+++ firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD291ne6iaNec4liqRxkXrwvvIM3jfnzHs",
-  authDomain: "react-auth-e3a95.firebaseapp.com",
-  projectId: "react-auth-e3a95",
-  storageBucket: "react-auth-e3a95.appspot.com",
-  messagingSenderId: "95707904102",
-  appId: "1:95707904102:web:0c3be05208b87fc43f55a7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
