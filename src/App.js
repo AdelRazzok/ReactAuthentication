7251a1ef60f2b6779612efc7b7db12d9c9a4419d
= <LHP6.arazzok@gmail.com>
2022-01-28 18:10:22 +0100

    add sign up modal and user context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Signup from './components/Signup'
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
+			<Signup />
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
---
+++ src/components/Signup.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export default function Signup() {
+	return (
+		<>
+			<div className="position-fixed top-0 vw-100 vh-100">
+				<div className="w-100 h-100 bg-dark bg-opacity-75">
+					<div className="position-absolute top-50 start-50 translate-middle" style={{ minWidth: "400px" }}>
+						<div className="modal-dialog">
+							<div className="modal-content">
+								<div className="modal-header">
+									<h5 className="modal-title">Sign up</h5>
+									<div className="btn-close"></div>
+								</div>
+								<div className="modal-body">
+									<form className="sign-up-form">
+										<div className="mb-3">
+											<label htmlFor="signUpEmail" className='form-label'>Email adress</label>
+											<input
+												type='email'
+												name='email' 
+												className='form-control'
+												required
+												id='signUpEmail'
+											/>
+										</div>
+
+										<div className="mb-3">
+											<label htmlFor="signUpPwd" className='form-label'>Password</label>
+											<input
+												type='password'
+												name='pwd'
+												className='form-control'
+												required
+												id='signUpPwd'
+											/>
+										</div>
+
+										<div className="mb-3">
+											<label htmlFor="repeatPwd" className='form-label'>Repeat Password</label>
+											<input
+												type='password'
+												name='pwd'
+												className='form-control'
+												required
+												id='repeatPwd'
+											/>
+											<p className='text-danger mt-1'></p>
+										</div>
+
+										<button className="btn btn-primary mt-2">Submit</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
